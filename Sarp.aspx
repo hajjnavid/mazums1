7ff0facb5400cb6fe9467abe53fdf7cbcba175e8
hajjnavid <146057793+hajjnavid@users.noreply.github.com>
2023-11-04 11:21:13 +0330

    Add files via upload

---
+++ Sarp.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPageNull.master" AutoEventWireup="true" CodeFile="Sarp.aspx.cs" Inherits="Sarp" %>
+<%@ Register  Src="~/PopUpMessage.ascx" TagName="Pop" TagPrefix="Elpt" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+ <Elpt:Pop ID="pop" runat="server" />
+
+
+    <div class="container">
+        <div class="boxNew1">
+            <div class="boxNew1-header bold">فرم تعریف</div>
+            <div class="boxNew1-body">
+                <div class="row">
+                    
+                     
+
+                <div class="col-sm-3">
+                   <div class="ColorBBB">بسته</div>
+                    <asp:DropDownList ID="ddl_req" runat="server" CssClass="dropdown"></asp:DropDownList>
+                  </div>
+
+
+
+                 <div class="col-sm-3">
+                   <div class="ColorBBB">بسته</div>
+                   <asp:CheckBoxList  ID="chk_req" runat="server"  CssClass="checkbox-inline "></asp:CheckBoxList>
+
+                  </div>
+
+
+                </div>
+                </div>
+
+                <div style="text-align: center; padding-top: 40px">
+                    <asp:Button ID="btn" runat="server" BackColor="#00ff00" CssClass="btn btn-success"  OnClick="btn_Click" Text="ذخیره سازی" />
+                </div>
+
+                <div id="div_error" runat="server"></div>
+
+            </div>
+        </div>
+
+    
+
+</asp:Content>
+
+
+
---
+++ Sarp.aspx.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using RestSharp.Extensions;
+
+public partial class Sarp : System.Web.UI.Page
+{
+    Ado_Class con = new Ado_Class();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        con.connect(Application["StrCon"].ToString());
+        Ado_Class.strcon = Application["StrCon"].ToString();
+        if (!Page.IsPostBack)
+        {
+            con.FillCheckBoxList(chk_req, "select id,value  from tbl_BulidReq");
+            con.FillDropdownList(ddl_req, "select id,value from tbl_bastebandi");
+        }
+    }
+
+    protected void btn_Click(object sender, EventArgs e)
+    {
+
+        try
+        {
+            con.open();
+            con.cammand();
+            for (int i = 0; i < chk_req.Items.Count; i++)
+            {
+                if (chk_req.Items[i].Selected)
+                
+                con.commandtext("Insert into tbl_AutoPlaceCare values (@0,@1)");
+                con.setcommandInt(0, ddl_req.ID);
+                con.setcommandInt(1, chk_req.Items[i].Text);
+                con.addcommand();
+               
+                
+            }
+            con.close();
+            string User = ddl_req.Text;
+            pop.PopOnShow(User + " " + "به ثبت رسیده است");
+
+        }
+        catch (Exception ex)
+        {
+            pop.PopOnShow("Error" + ex.Message);
+        }
+       ;
+    }
+
+    }
